8bc2cb93952226582a1002e06f4997b5b523150c
Chiu, Po-Yen <87821074+DALBrian@users.noreply.github.com>
2025-09-07 15:05:44 +0800

    1. Add WiFi configuration to ESP-IDF Menuconfig. 2. Integrate MQTT and DHT sensor.

--- main/main.c
+++ main/main.c
@@ -11,15 +11,24 @@
 #include "esp_netif.h"
 #include "protocol_examples_common.h"
 #include "mqtt_client.h"
-#define MQTT_TOPIC "iot/esp32-01/telemetry/dht"
+#include <time.h>
+
+#define MQTT_TOPIC "Test"
 
 static const char *TAG = "Project: NodeMCU-32S-Iot";
 
 static esp_mqtt_client_handle_t g_mqtt = NULL;
 static volatile bool g_mqtt_connected = false;
+static esp_mqtt5_user_property_item_t user_property_arr[] = {
+        {"board", "esp32"},
+        {"u", "user"},
+        {"p", "password"}
+    };
 
+#define USE_PROPERTY_ARR_SIZE   sizeof(user_property_arr)/sizeof(esp_mqtt5_user_property_item_t)
 void DHT_task(void *pvParameter)
 {
+    static uint16_t seq = 0U;
     setDHTgpio(GPIO_NUM_25);
     ESP_LOGI(TAG, "Starting DHT Task\n\n");
 
@@ -34,6 +43,8 @@ void DHT_task(void *pvParameter)
         float tmp = getTemperature();
         ESP_LOGI(TAG, "Hum: %.1f Tmp: %.1f", hum, tmp);
 
+        // esp_mqtt_client_publish(g_mqtt, "Test", "hello-from-esp32", 0, 1, 0);
+
         char ts[32];
         time_t now = time(NULL);
         struct tm tm_utc;
@@ -46,25 +57,27 @@ void DHT_task(void *pvParameter)
                          ts, tmp, hum, seq++);
 
         if (g_mqtt && g_mqtt_connected && n > 0) {
-            int msg_id = esp_mqtt_client_publish(
-                g_mqtt, MQTT_TOPIC, payload, 0,
-                /*QoS*/1, /*retain*/0);
+            int msg_id = esp_mqtt_client_publish(g_mqtt, MQTT_TOPIC, payload, 0, 1, 0);
             ESP_LOGI(TAG, "MQTT publish [%s] id=%d payload=%s", MQTT_TOPIC, msg_id, payload);
         } else {
             ESP_LOGW(TAG, "MQTT not connected, skip publish");
-        }     
-
-        // The interval of whole process must be beyond 2 seconds !!
+        }
+        ESP_LOGI(TAG, "DHT task high-water mark: %u bytes", uxTaskGetStackHighWaterMark(NULL));
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
 
-static void mqtt_event_handler(void *args, esp_event_base_t base, int32_t id, void *data) {
-    esp_mqtt_event_handle_t e = (esp_mqtt_event_handle_t)data;
-    switch (id) {
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    g_mqtt = event->client;
+
+    switch ((esp_mqtt_event_id_t)event_id) {
         case MQTT_EVENT_CONNECTED:
             g_mqtt_connected = true;
             ESP_LOGI(TAG, "MQTT connected");
+            
             break;
         case MQTT_EVENT_DISCONNECTED:
             g_mqtt_connected = false;
@@ -75,15 +88,53 @@ static void mqtt_event_handler(void *args, esp_event_base_t base, int32_t id, vo
     }
 }
 
-static void mqtt_start(void) {
-    const esp_mqtt_client_config_t cfg = {
+static void mqtt5_app_start(void)
+{
+    printf("mqtt5_app_start. \r\n");
+    esp_mqtt5_connection_property_config_t connect_property = {
+        .session_expiry_interval = 10,
+        .maximum_packet_size = 1024,
+        .receive_maximum = 65535,
+        .topic_alias_maximum = 2,
+        .request_resp_info = true,
+        .request_problem_info = true,
+        .will_delay_interval = 10,
+        .payload_format_indicator = true,
+        .message_expiry_interval = 10,
+        .response_topic = "/test/response",
+        .correlation_data = "123456",
+        .correlation_data_len = 6,
+    };
+
+    esp_mqtt_client_config_t mqtt5_cfg = {
         .broker.address.uri = CONFIG_BROKER_URL,
         .session.protocol_ver = MQTT_PROTOCOL_V_5,
         .network.disable_auto_reconnect = true,
+        .credentials.username = "123",
+        .credentials.authentication.password = "456",
+        .session.last_will.topic = "/topic/will",
+        .session.last_will.msg = "i will leave",
+        .session.last_will.msg_len = 12,
+        .session.last_will.qos = 1,
+        .session.last_will.retain = true,
     };
-    g_mqtt = esp_mqtt_client_init(&cfg);
-    esp_mqtt_client_register_event(g_mqtt, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-    esp_mqtt_client_start(g_mqtt);
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt5_cfg);
+
+    /* Set connection properties and user properties */
+    esp_mqtt5_client_set_user_property(&connect_property.user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+    esp_mqtt5_client_set_user_property(&connect_property.will_user_property, user_property_arr, USE_PROPERTY_ARR_SIZE);
+    esp_mqtt5_client_set_connect_property(client, &connect_property);
+
+    /* If you call esp_mqtt5_client_set_user_property to set user properties, DO NOT forget to delete them.
+     * esp_mqtt5_client_set_connect_property will malloc buffer to store the user_property and you can delete it after
+     */
+    esp_mqtt5_client_delete_user_property(connect_property.user_property);
+    esp_mqtt5_client_delete_user_property(connect_property.will_user_property);
+
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
 }
 
 
@@ -92,8 +143,6 @@ void app_main()
 {
     ESP_LOGI(TAG, "Startup...");
     ESP_LOGI(TAG, "Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
-
-
 
     esp_log_level_set("*", ESP_LOG_INFO);
     esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
@@ -104,7 +153,7 @@ void app_main()
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect()); 
 
-    mqtt_start();
-    xTaskCreate(&DHT_task, "DHT_task", 2048, NULL, 5, NULL);
+    mqtt5_app_start();
+    xTaskCreate(&DHT_task, "DHT_task", 4096, NULL, 5, NULL);
 }
 
